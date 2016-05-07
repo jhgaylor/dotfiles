d97d9b0c306114658f0c0e4ffb40b1c504348691
Jake Gaylor <jgaylor@fsw.com>
2016-05-07 11:29:32 -0600

    add kubernetes aliases and some love for bash completion

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
+  source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -20,3 +20,7 @@ else
 	fi;
 fi;
 unset doIt;
+
+echo "You should do some stuff to make your mac awesome now."
+echo "Review this if you need to: http://davidalger.com/development/bash-completion-on-os-x-with-brew/"
+echo "brew install bash-completion"
